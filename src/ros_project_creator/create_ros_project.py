138d1626edc781aa42e20248940c264753eb5fb3
juan.rascon <juan.rascon@eurecat.org>
2025-07-23 15:13:08 +0000

    Format create_ros_project.py with new ruff rules

--- src/ros_project_creator/create_ros_project.py
+++ src/ros_project_creator/create_ros_project.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 import argparse
-import argcomplete
 import os
-from pathlib import Path
 import sys
+from pathlib import Path
 
-from ros_project_creator.ros_project_creator import RosProjectCreator
-from ros_project_creator.ros_project_creator import RosProjectCreatorException
+import argcomplete
+
+from ros_project_creator.ros_project_creator import RosProjectCreator, RosProjectCreatorException
 from ros_project_creator.utilities import Utilities
 
 
 def main():
     try:
         if os.geteuid() == 0:
-            raise RuntimeError("ERROR: This script must not be run with sudo or as root")
+            raise RuntimeError('ERROR: This script must not be run with sudo or as root')
 
-        resources_dir = Path(__file__).parent.joinpath("resources")
-        ros_variants = Utilities.load_yaml(resources_dir.joinpath("ros", "ros_variants.yaml"))
+        resources_dir = Path(__file__).parent.joinpath('resources')
+        ros_variants = Utilities.load_yaml(resources_dir.joinpath('ros', 'ros_variants.yaml'))
         Utilities.assert_non_empty(
             ros_variants, f"No ROS variants found in the resource path '{resources_dir.resolve()}'"
         )
-        supported_ros_distros = ", ".join(
-            f"{ros_distro} (ros{data['ros_version']})" for ros_distro, data in ros_variants.items()
+        supported_ros_distros = ', '.join(
+            f'{ros_distro} (ros{data["ros_version"]})' for ros_distro, data in ros_variants.items()
         )
 
         parser = argparse.ArgumentParser(
-            description="Creates a new ROS project based on templates",
+            description='Creates a new ROS project based on templates',
             allow_abbrev=False,  # Disable prefix matching
             add_help=False,  # Add custom help message
             formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, max_help_position=40),
@@ -33,62 +33,59 @@ def main():
         )
 
         parser.add_argument(
-            "project_id", type=str, help="Short, descriptive project identifier for internal reference (e.g. 'robproj')"
+            'project_id', type=str, help="Short, descriptive project identifier for internal reference (e.g. 'robproj')"
         )
 
         parser.add_argument(
-            "project_dir",
-            type=str,
-            help="Path where the project will be created (e.g. '~/projects/robproj')",
+            'project_dir', type=str, help="Path where the project will be created (e.g. '~/projects/robproj')"
         )
 
-        parser.add_argument("base_img", type=str, help="Base Docker image to use (e.g. 'eutrob/eut_ros:humble')")
+        parser.add_argument('base_img', type=str, help="Base Docker image to use (e.g. 'eutrob/eut_ros:humble')")
+
+        parser.add_argument('img_user', type=str, help='Active user to use in the resulting Docker image')
+
+        parser.add_argument('ros_distro', type=str, help=f'ROS distro to use: {supported_ros_distros}')
 
         parser.add_argument(
-            "img_user",
-            type=str,
-            help="Active user to use in the resulting Docker image",
-        )
-
-        parser.add_argument("ros_distro", type=str, help=f"ROS distro to use: {supported_ros_distros}")
-
-        parser.add_argument(
-            "--img-id",
+            '--img-id',
             type=str,
             default=None,
-            help="ID of the resulting Docker image (e.g. 'robproj:humble'). If not set, defaults to '<project-id>:latest'",
+            help=(
+                "ID of the resulting Docker image (e.g. 'robproj:humble'). If not set, defaults to "
+                "'<project-id>:latest'"
+            ),
         )
 
         parser.add_argument(
-            "--use-base-img-entrypoint",
-            action="store_true",
+            '--use-base-img-entrypoint',
+            action='store_true',
             help="The image will inherit the base image's entrypoint, if any",
         )
 
         parser.add_argument(
-            "--no-environment",
-            action="store_true",
-            help="Do not use an environment script. Do not use this option if you set a custom environment script",
+            '--no-environment',
+            action='store_true',
+            help='Do not use an environment script. Do not use this option if you set a custom environment script',
         )
 
-        parser.add_argument("--use-host-nvidia-driver", action="store_true", help="Use host's NVIDIA driver")
+        parser.add_argument('--use-host-nvidia-driver', action='store_true', help="Use host's NVIDIA driver")
 
-        parser.add_argument("--no-vscode", action="store_true", help="Do not create VSCode project")
+        parser.add_argument('--no-vscode', action='store_true', help='Do not create VSCode project')
 
-        parser.add_argument("--no-pre-commit", action="store_true", help="Do not use pre-commit hooks")
+        parser.add_argument('--no-pre-commit', action='store_true', help='Do not use pre-commit hooks')
 
         parser.add_argument(
-            "--no-console-log",
-            action="store_true",
-            help="Disable logging to console. Console logging is enabled by default",
+            '--no-console-log',
+            action='store_true',
+            help='Disable logging to console. Console logging is enabled by default',
         )
 
-        parser.add_argument("--log-file", type=str, help="File to log output", default="")
+        parser.add_argument('--log-file', type=str, help='File to log output', default='')
 
-        parser.add_argument("--log-level", type=str, help="Logging level (Default is DEBUG)", default="DEBUG")
+        parser.add_argument('--log-level', type=str, help='Logging level (Default is DEBUG)', default='DEBUG')
 
         parser.add_argument(
-            "-h", "--help", action="help", default=argparse.SUPPRESS, help="Show this help message and exit"
+            '-h', '--help', action='help', default=argparse.SUPPRESS, help='Show this help message and exit'
         )
 
         argcomplete.autocomplete(parser)
@@ -113,10 +110,10 @@ def main():
     except RosProjectCreatorException:
         sys.exit(1)
     except Exception as e:
-        print(f"{e}", file=sys.stderr)
+        print(f'{e}', file=sys.stderr)
         # traceback.print_exc()
         sys.exit(1)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
